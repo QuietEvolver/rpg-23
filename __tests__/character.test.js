ef2924e329ae9ce017889b81b1e1fab2a726b504
Ashe Urban <ashe@goldentongue.com>
2023-02-01 14:22:51 -0800

    Pass tests for healthPlusOne and healthMinusOne.
    Co-authored-by: Mitchell Gantz <mgantz22@icloud.com>
    Co-authored by: Mesha <madsketchz@gmail.com>
    Co-authored by : Erin Timlin <erintimlin@gmail.com
    Co-authored-by: Vera Weikel <vera.weikel@gmail.com>

--- __tests__/character.test.js
+++ __tests__/character.test.js
@@ -2,11 +2,30 @@ import Character from '../src/js/character.js';
 
 describe('Character', () => {
 
-  test('should correctly create a object object with three properties', () => {
+  test('should correctly create a character object with a health property', () => {
     const character = new Character(12);
-
     expect(character.health).toEqual(12);
   });
 
-  
+  test('should add 1 to health score', () => {
+    const character = new Character(12);
+    expect(character.healthPlusOne()).toEqual(13);
+  });
+
+  test("should return 0 if health score is 0", () => {
+    const character = new Character(0);
+    expect(character.healthPlusOne()).toEqual(0);
+  });
+
+  test('should minus 1 to health score', () => {
+    const character = new Character(12);
+    expect(character.healthMinusOne()).toEqual(11);
+  });
+
+  test("should return 0 if health score is 0", () => {
+    const character = new Character(0);
+    expect(character.healthMinusOne()).toEqual(0);
+  });
+
+
 });
--- src/js/character.js
+++ src/js/character.js
@@ -1,8 +1,23 @@
 
 export default class Character {
-  constructor(health){
+  constructor(health) {
     this.health = health;
-
   }
 
+  healthPlusOne() {
+    if(this.health !== 0) {
+    return this.health += 1;
+    } else {
+      return this.health;
+    }
+  }
+
+  healthMinusOne() {
+    if(this.health !== 0) {
+      return this.health -= 1;
+    } else {
+      return this.health;
+    }
+  }
+    
 };
